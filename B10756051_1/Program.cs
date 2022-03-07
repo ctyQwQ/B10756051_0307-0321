3f5913c8d5ee2f2eeb263a98b07020a980b0b6dc
ctyQwQ <cty20000216@gmail.com>
2022-03-07 11:55:44 +0800

    完成

--- B10756051_1/Program.cs
+++ B10756051_1/Program.cs
@@ -6,7 +6,12 @@ namespace B10756051_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double meal_cost = 100;
+            int tip_percent = 15;
+            int tax_percent = 8;
+
+            double totle = meal_cost + tip_percent + tax_percent;
+            Console.WriteLine(totle);
         }
     }
 }
