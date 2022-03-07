4bbe5bab5eb2b56ade937c587106277af74120da
ctyQwQ <cty20000216@gmail.com>
2022-03-07 11:31:49 +0800

    加入專案檔案。

---
+++ B10756051_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace B10756051_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
