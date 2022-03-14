a18f9d9960e340d9c9a8dd48321300376a82c09a
ctyQwQ <cty20000216@gmail.com>
2022-03-14 18:21:37 +0800

    完成

--- B10756051_1/B10756051.cs
+++ B10756051_1/B10756051.cs
@@ -7,17 +7,21 @@ namespace B10756051_1
         static void Main(string[] args)
         {
             //0307 作業
+            Console.WriteLine("餐費100元，服務費15%，稅8%：");
             double meal_cost = 100;
             int tip_percent = 15;
             int tax_percent = 8;
 
-            double totle = meal_cost * 1.15 * 1.08;
+            double tip = tip_percent * 0.01;
+            double tax = tax_percent * 0.01;
+
+            double totle = meal_cost + meal_cost * tip + (meal_cost + meal_cost * tip) * tax;
             Console.WriteLine(totle);
 
 
 
             //0314 練習1
-            Console.Write("\n請輸入數字:");
+            Console.Write("\n\n請輸入數字：");
             string input = Console.ReadLine();
             int inputInt = int.Parse(input);
 
@@ -39,7 +43,7 @@ namespace B10756051_1
             }
 
             //0314 練習2
-            Console.Write("\n請輸入數字:");
+            Console.Write("\n\n請輸入數字：");
             input = Console.ReadLine();
             inputInt = int.Parse(input);
 
@@ -52,8 +56,10 @@ namespace B10756051_1
                 Console.WriteLine();
             }
 
+
+
             //0314 練習3
-            Console.Write("\n請輸入數字:");
+            Console.Write("\n\n請輸入數字：");
             input = Console.ReadLine();
             inputInt = int.Parse(input);
 
@@ -64,39 +70,52 @@ namespace B10756051_1
                 Console.WriteLine($"{inputInt} * {i} = {num}");
             }
 
+
+
             //0314 練習4
-            for (int i = 2; i <= 9; i++)
+            Console.WriteLine("\n九九乘法表：");
+            for (int i = 1; i <= 9; i++)
             {
-                for (int j = 1; j <= 9; j++)
+                for (int j = 2; j <= 9; j++)
                 {
-                    Console.WriteLine(i + "*" + j + "=" + i * j);
+                    int num = i * j;
+                    Console.Write($"{j} * {i} = {num, 2}    ");
                 }
                 Console.WriteLine();
             }
 
+
+
             //0314 練習5
-            int[] studenNo = { 1, 2, 3, 4, 5 };
-            string[] studenName = { "Jack", "Mary", "Tom", "Grace", "Alice" };
+            int[] sNo = { 1, 2, 3, 4, 5 };
+            string[] sName = { "Jack", "Mary", "Tom", "Grace", "Alice" };
             int[] computerScore = { 80, 65, 100, 98, 83 };
             int[] progamScore = { 75, 67, 93, 25, 82 };
             int[] animationScore = { 60, 62, 91, 50, 87 };
 
-            while (true)
+            while ( true )
             {
-                Console.WriteLine("請輸入座號(1~5):");
+                Console.WriteLine("\n\n請輸入座號(1~5)查詢成績, 輸入\"0\"退出。");
                 input = Console.ReadLine();
                 int inputStudenNo = int.Parse(input);
-                int arrayIndex = Array.IndexOf(studenNo, inputStudenNo);
+                int arrayIndex = Array.IndexOf(sNo, inputStudenNo);
+
+                if (inputStudenNo == 0)
+                {
+                    Console.WriteLine("成功退出！");
+                    break;
+                }
+
                 if (arrayIndex > -1)
                 {
-                    Console.WriteLine($"姓名:{studenName[arrayIndex]}");
-                    Console.WriteLine($"電腦概論:{computerScore[arrayIndex]}");
-                    Console.WriteLine($"程式設計:{progamScore[arrayIndex]}");
-                    Console.WriteLine($"動畫設計:{animationScore[arrayIndex]}");
+                    Console.WriteLine($"姓名：{sName[arrayIndex]}");
+                    Console.WriteLine($"電腦概論：{computerScore[arrayIndex]}分");
+                    Console.WriteLine($"程式設計：{progamScore[arrayIndex]}分");
+                    Console.WriteLine($"動畫設計：{animationScore[arrayIndex]}分");
                 }
                 else
                 {
-                    Console.WriteLine("沒有這個座號");
+                    Console.WriteLine("查無此座號！\n");
                 }
             }
         }
